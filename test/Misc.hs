d2f149c29aad88b8f36ac50c0399f335db0ed929
Taras Serduke <taras.serduke@gmail.com>
2016-09-15 21:21:07 +0300

    card bound test

--- test/Misc.hs
+++ test/Misc.hs
@@ -9,7 +9,13 @@ import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $
-  describe "Card" $
+  describe "Card" $ do
+    it "minBound" $ do
+      cardRank minBound `shouldBe` R2
+      cardSuit minBound `shouldBe` Clubs
+    it "maxBound" $ do
+      cardRank maxBound `shouldBe` Ace
+      cardSuit maxBound `shouldBe` Spades
     it "toEnum error" $ do
       evaluate (toEnum (-1) :: Card) `shouldThrow` errorCall "Card toEnum -1"
       evaluate (toEnum 52   :: Card) `shouldThrow` errorCall "Card toEnum 52"
